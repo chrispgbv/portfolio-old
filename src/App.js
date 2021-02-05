742e1d0f7f0628d517d114609a6250f6e4d9da7b
chrispgbv <christine.valkner@gmail.com>
2021-02-05 12:13:42 +0100

    created front page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Page1 from './components/page1.js';
+import './style.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        < Page1/>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
